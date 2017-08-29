bff5a7ece37bbb04683806747038621cb0bf6667
danmyasnikov <daniel.g.myasnikov@gmail.com>
2017-08-29 17:04:33 +1000

    added blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root controller: :static, action: 'public/index.html'
 
+  get 'blog',            controller: :static, action: 'blog.html'
+  get 'blog/85-article', controller: :static, action: 'blog/85-article.html'
+
   namespace :api do
     resources :contacts, only: :create
   end
